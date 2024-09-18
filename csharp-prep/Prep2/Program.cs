e933e71150714eaa5e753a222cbbe67f893ae4ea
loisemusungu <mwongelimusungu@gmail.com>
2024-09-18 12:04:41 +0300

    complete the three core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade in percentage? ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+
+        string letter = "";
+
+        if (gradeInt >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeInt >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeInt >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You failed the course.");
+        }
     }
 }
