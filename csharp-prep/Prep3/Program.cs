283abcc6ddce673fe7c4e55771882e07bf3cac37
loisemusungu <mwongelimusungu@gmail.com>
2024-09-19 08:59:41 +0300

    added the condition

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int magicNumberInt = int.Parse(magicNumber);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guessInt = int.Parse(guess);
+        
+
+        if (guessInt == magicNumberInt)
+        {
+            Console.WriteLine("You guessed the magic number!");
+        }
+        if (guessInt < magicNumberInt)
+        {
+            Console.WriteLine("Higher.");
+        }
+        if (guessInt > magicNumberInt)
+        {
+            Console.WriteLine("Lower.");
+        }
     }
 }
