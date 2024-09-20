cf82c82a1835b1cae9fada6a1341e39f5abac2c1
loisemusungu <mwongelimusungu@gmail.com>
2024-09-20 09:33:49 +0300

    added 5 functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,40 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int userFavoriteNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, userNumber, userFavoriteNumber);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter a number: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static int SquareNumber(int userNumber)
+        {
+            return userNumber * userNumber;
+        }
+
+        static void DisplayResult(string userName, int userNumber, int userFavoriteNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {userFavoriteNumber}");
+        }   
     }
 }
