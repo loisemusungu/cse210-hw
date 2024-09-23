cd8235a6b3bca51b1d79e20f0a5d7efb3aa39ca9
loisemusungu <mwongelimusungu@gmail.com>
2024-09-23 12:59:20 +0300

    Created job and resume classes

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+using System;
+
+    public class Job
+    {
+        public string _company;
+        public string _title;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_title} ({_company}) {_startYear} - {_endYear}");
+        }
+    }
