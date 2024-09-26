37c6bdd7c6caac613ef1e3f02f6e5de3642e7d14
loisemusungu <mwongelimusungu@gmail.com>
2024-09-26 11:18:06 +0300

    Added loops in Journal.cs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,24 +1,55 @@
+using System;
+using System.IO;
+
 public class Journal
 {
     public List<Entry> _entries;
 
+    public Journal()
+    {
+        _entries = new List<Entry>();
+    }
+
     public void AddEntry(Entry newEntry)
     {
-
+        _entries.Add(newEntry);
     }
 
     public void DisplayAll()
     {
-
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
     }
 
     public void SaveToFile(string file)
     {
-
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date} ~ {entry._promptText} ~ {entry._entryText}");
+            }
+        }
     }
 
     public void LoadFromFile(string file)
     {
+        _entries.Clear();
 
+        string[] lines = File.ReadAllLines(file);
+
+        for(int i = 0; i < lines.Length; i++)
+        {
+            string[] parts = lines[i].Split('~');
+            DateTime date = DateTime.Parse(parts[0].Trim());
+            string promptText = parts[1].Trim();
+            string entryText = parts[2].Trim();
+
+            Entry entry = new Entry(promptText, entryText);
+            entry._date = date;
+            _entries.Add(entry);
+        }
     }   
 }
