d7ed193706a6ebf2ff6abc8eb77f0dc2b890fc7b
loisemusungu <mwongelimusungu@gmail.com>
2024-09-26 11:05:53 +0300

    added public Entry and public void

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,13 +1,20 @@
 public class Entry
 {
-    public string _date;
+    public DateTime _date;
     public string _promptText;
     public string _entryText;
 
+    public Entry(string promptText, string entryText)
+    {
+        _date = DateTime.Now;
+        _promptText = promptText;
+        _entryText = entryText;
+    }
+
     public void Display()
     {
-        Console.WriteLine(_date);
-        Console.WriteLine(_promptText);
-        Console.WriteLine(_entryText);
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Prompt: {_promptText}");
+        Console.WriteLine($"Response: {_entryText}");
     }
 }
