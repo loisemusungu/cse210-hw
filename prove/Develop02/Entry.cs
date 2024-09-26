df6b94cfd077c11884ea0e0ff7c509a7004a6ea4
loisemusungu <mwongelimusungu@gmail.com>
2024-09-26 10:55:47 +0300

    addeed console.write lines

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -6,6 +6,8 @@ public class Entry
 
     public void Display()
     {
-        
+        Console.WriteLine(_date);
+        Console.WriteLine(_promptText);
+        Console.WriteLine(_entryText);
     }
 }
