7d4c37590de706a68e705b909c9e906074605e8e
loisemusungu <mwongelimusungu@gmail.com>
2024-09-30 12:02:12 +0300

    added different fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,13 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Fraction f1 = new Fraction();
-        Fraction f2 = new Fraction(3, 4);
-        Fraction f3 = new Fraction(3, "/", 4);
-        
-        Console.WriteLine(f1);
-        Console.WriteLine(f2);
-        Console.WriteLine(f3);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
 
-        Console.ReadLine();
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3,4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+        
     }
 }
