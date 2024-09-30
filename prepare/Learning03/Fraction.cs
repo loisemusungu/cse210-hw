21894877c4c39b1e992fd3f54f50ad17f10628c8
loisemusungu <mwongelimusungu@gmail.com>
2024-09-30 11:13:10 +0300

    added the fraction class with two private attributes

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,12 @@
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+
+    }
+}
