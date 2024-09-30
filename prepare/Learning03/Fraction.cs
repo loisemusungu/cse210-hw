5e00e899d941e5f57ec4b42b3a49aff76f7c3c85
loisemusungu <mwongelimusungu@gmail.com>
2024-09-30 11:28:23 +0300

    added three constructors

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -3,10 +3,26 @@ public class Fraction
     private int _numerator;
     private int _denominator;
 
+    private string _divider;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 5;
+        _divider = "/";
+
+    }
+
     public Fraction(int numerator, int denominator)
     {
         _numerator = numerator;
         _denominator = denominator;
+    }
 
+    public Fraction(int numerator, string divider, int denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+        _divider = divider;
     }
 }
