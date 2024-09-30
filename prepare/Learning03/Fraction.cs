9c33a62b2a5a68a3caca1955e8d42d77d10fd593
loisemusungu <mwongelimusungu@gmail.com>
2024-09-30 12:06:04 +0300

    removed setters

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -4,50 +4,36 @@ public class Fraction
     private int _numerator;
     private int _denominator;
 
-    private string _divider;
 
     public Fraction()
     {
         _numerator = 1;
-        _denominator = 5;
-        _divider = "/";
+        _denominator = 1;
 
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
     }
 
     public Fraction(int numerator, int denominator)
     {
         _numerator = numerator;
         _denominator = denominator;
-    }
 
-    public Fraction(int numerator, string divider, int denominator)
-    {
-        _numerator = numerator;
-        _denominator = denominator;
-        _divider = divider;
     }
 
     public string GetFractionString()
     {
-        string text = _numerator + _divider + _denominator;
+        string text = $"{_numerator}/{_denominator}";
         return text;
-    }
-
-    public void SetFractionString(int numerator, string divider, int denominator)
-    {
-        _numerator = numerator;
-        _denominator = denominator;
-        _divider = divider;
     }
 
     public double GetDecimalValue()
     {
-        return (double)_numerator / _denominator;
+        return (double)_numerator / (double)_denominator;
     }
 
-    public void SetDecimalValue(double value)
-    {
-        _numerator = (int)(value * 100);
-        _denominator = 100;
-    }
 }
