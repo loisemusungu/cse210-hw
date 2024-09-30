de4db8265f64ea838daee310609a30259ce1b84a
loisemusungu <mwongelimusungu@gmail.com>
2024-09-30 11:53:02 +0300

    added getters and setters

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(3, 4);
+        Fraction f3 = new Fraction(3, "/", 4);
+        
+        Console.WriteLine(f1);
+        Console.WriteLine(f2);
+        Console.WriteLine(f3);
+
+        Console.ReadLine();
     }
 }
