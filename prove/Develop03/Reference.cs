5911b4849b8a7fbe187185c008577d7bcc350396
loisemusungu <mwongelimusungu@gmail.com>
2024-10-03 20:02:40 +0300

    added the objects, methods and function

---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,12 @@
+using System;
+
+_book: string;
+_chapter: int;
+_verse: int;
+_endVerse: int;
+
+Reference(book : string, chapter : int, verse : int);
+
+Reference(book : string, chapter : int, verse : int, endVerse : int);
+
+GetDisplayText(): string;
