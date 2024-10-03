f3453fe52b37204cc8c63cbc471c36250a8dda10
loisemusungu <mwongelimusungu@gmail.com>
2024-10-03 20:07:05 +0300

    added objects. constructors and functions

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,44 @@
+using System;
+
+_text: string;
+_isHidden: bool;
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            return "______";
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
+
+
