1ce8f254e4e7eef4c9553d019a85c7222de189b0
loisemusungu <mwongelimusungu@gmail.com>
2024-10-04 08:23:24 +0300

    added a scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.");  
+        scripture.HideRandomWords(3);
+        Console.WriteLine(scripture.GetDisplayText());
+
         Console.WriteLine("Hello Develop03 World!");
     }
 }
