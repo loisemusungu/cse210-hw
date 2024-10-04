dcc87f58847b011dd90040e8a0debecec6859983
loisemusungu <mwongelimusungu@gmail.com>
2024-10-04 08:17:47 +0300

    added private elements and methods

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,12 +1,48 @@
 using System;
 
-_book: string;
-_chapter: int;
-_verse: int;
-_endVerse: int;
+// _book: string;
+// _chapter: int;
+// _verse: int;
+// _endVerse: int;
 
-Reference(book : string, chapter : int, verse : int);
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
 
-Reference(book : string, chapter : int, verse : int, endVerse : int);
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = verse;
+    }
 
-GetDisplayText(): string;
+    public Reference(string book, int chapter, int verse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_endVerse == _verse)
+        {
+            return $"{_book} {_chapter}:{_verse}";
+        }
+        else
+        {
+            return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        }
+    }
+}
+
+// Reference(book : string, chapter : int, verse : int);
+
+// Reference(book : string, chapter : int, verse : int, endVerse : int);
+
+// GetDisplayText(): string;
