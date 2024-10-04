b302f5bbc4a02519884ee6e9e01ee9ccf59ccb47
loisemusungu <mwongelimusungu@gmail.com>
2024-10-04 09:16:21 +0300

    I have added a new scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -23,7 +23,8 @@ class Program
             new Scripture(new Reference("John", 3, 16), "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life."),
             new Scripture(new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight."),
             new Scripture(new Reference("Psalms", 23, 1), "The Lord is my shepherd; I shall not want."),
-            new Scripture(new Reference("Romans", 8, 28), "And we know that in all things God works for the good of those who love him, who have been called according to his purpose.")
+            new Scripture(new Reference("Romans", 8, 28), "And we know that in all things God works for the good of those who love him, who have been called according to his purpose."),
+            new Scripture(new Reference("Psalms", 100, 1, 2), "Make a joyful noise to the Lord, all the earth! Serve the Lord with gladness! Come into his presence with singing!")
         };
 
         Random random = new Random();
