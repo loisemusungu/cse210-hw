13789127e230a2c0f130da8ce50512cdc58bc23e
loisemusungu <mwongelimusungu@gmail.com>
2024-10-08 13:32:51 +0300

    added .addcomment function

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -9,40 +9,29 @@ class Program
         video1._title = "Lessons from Steve Jobs";
         video1._author = "Steve Jobs";
         video1._length = 180;
-        //video1._comments = 2;
 
         Video video2 = new Video();
         video2._title = "Lessons from Bill Gates";
         video2._author = "Bill Gates";
         video2._length = 120;
-        //video2._comments = 3;
 
         Video video3 = new Video();
         video3._title = "Lessons from Elon Musk";
         video3._author = "Elon Musk";
         video3._length = 240;
-       // video3._comments = 4;
 
-        video1.Display();
-        Comment myComment = new Comment();
-        myComment._name = "Becky Smith";
-        myComment._text = "Great video!";
-        // myComment._comments.Add(myComment);
-        myComment.Display();
-
-        video2.Display();
-        Comment myComment2 = new Comment();
-        myComment2._name = "John Doe";
-        myComment2._text = "I love this video!";
-        // myComment2._comments.Add(myComment);
-        myComment2.Display();
+        video1.AddComment(new Comment { _name = "Becky Smith", _text = "Great video!" });
         
+        // Adding comments to video2
+        video2.AddComment(new Comment { _name = "John Doe", _text = "I love this video!" });
+        
+        // Adding comments to video3
+        video3.AddComment(new Comment { _name = "Jane Here", _text = "I learned a lot!" });
+
+        // Display videos and comments
+        video1.Display();
+        video2.Display();
         video3.Display();
-        Comment myComment3 = new Comment();
-        myComment3._name = "Jane Here";
-        myComment3._text = "I learned a lot!";
-        // myComment3._comments.Add(myComment);
-        myComment3.Display();
 
     }
 }
