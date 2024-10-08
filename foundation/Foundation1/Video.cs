4a0de43a5d24ebd75f41032bdeaf4464a1aa96ee
loisemusungu <mwongelimusungu@gmail.com>
2024-10-08 13:30:44 +0300

    added count fucntion to count the comments

--- foundation/Foundation1/Video.cs
+++ foundation/Foundation1/Video.cs
@@ -7,16 +7,30 @@ public class Video
     public string _author;
     public int _length;
 
-    public int _comments = 0;
-    
+    public List<Comment> _comments = new List<Comment>();
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
     public void Display()
     {
-        Console.WriteLine($"{_title} ({_length} seconds)");
+        Console.WriteLine($"{_title} by {_author} ({_length} seconds)");
+        Console.WriteLine($"Number of comments: {_comments.Count}"); // Display comment count
 
-        for (int i = 0; i < _comments; i++)
+        if (_comments.Count > 0)
         {
-            Console.WriteLine("Comment: " + (i + 1));
+            Console.WriteLine("Comments:");
+            foreach (Comment comment in _comments)
+            {
+                comment.Display();
+            }
         }
-
+        else
+        {
+            Console.WriteLine("No comments yet.");
+        }
+        Console.WriteLine(); // Add a blank line for better readability
     }
 }
