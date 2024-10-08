8f129b00b0e466d6264ff0d1036e74fe4f4413a3
loisemusungu <mwongelimusungu@gmail.com>
2024-10-08 13:31:39 +0300

    removed the comment list

--- foundation/Foundation1/Comment.cs
+++ foundation/Foundation1/Comment.cs
@@ -6,17 +6,9 @@ public class Comment
     public string _name;
     public string _text;
 
-    public List<Comment> _comments = new List<Comment>();
-
     public void Display()
     {
         Console.WriteLine($"Comments: \n {_name}: {_text} \n");
         
-
-        // Display each comment
-        foreach (Comment comment in _comments)
-        {
-            comment.Display();
-        }
     }
 }
