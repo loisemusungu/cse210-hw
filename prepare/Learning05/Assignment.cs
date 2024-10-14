ad76985c44f6584ee2a08a1969b3cf760a6810e3
loisemusungu <mwongelimusungu@gmail.com>
2024-10-14 11:58:44 +0300

    Added getters and setters

--- prepare/Learning05/Assignment.cs
+++ prepare/Learning05/Assignment.cs
@@ -6,6 +6,25 @@ public class Assignment
     private string _studentName = "";
     private string _topic = "";
 
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+
+    public void SetStudentName(string studentName)
+    {
+        _studentName = studentName;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public void SetTopic(string topic)
+    {
+        _topic = topic;
+    }
     public string GetSummary()
     {
         return $"{_studentName} is working on {_topic}";
