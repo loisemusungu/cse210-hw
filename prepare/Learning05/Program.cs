b76b0e358777c6e1731ef4ee1c0c2b3fb82d0e74
loisemusungu <mwongelimusungu@gmail.com>
2024-10-14 12:43:32 +0300

    added writing details

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,9 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Assignment assignment1 = new Assignment();
-        assignment1.SetStudentName("John");
-        assignment1.SetTopic("C#");
-        
+        assignment1.SetStudentName("Mary Waters");
+        assignment1.SetTopic("European History");
+
+        Writing writing1 = new Writing();
+        writing1.SetTopic("The Causes of World War II");
+        writing1.SetStudentName("Mary Waters");
+
         Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
 }
