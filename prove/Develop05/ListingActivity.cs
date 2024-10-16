cb4441ff0b5b32569e8e736dc244dff0fd6923b4
loisemusungu <mwongelimusungu@gmail.com>
2024-10-16 12:52:58 +0300

    added private variables and methods

---
+++ prove/Develop05/ListingActivity.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class ListingActivity
+{
+    private int _count = 0;
+    private List<string> _list = new List<string>();
+
+    ListingActivity()
+    {}
+
+    public void run()
+    {}
+
+    public void GetRandomPrompt()
+    {}
+
+    public void GetListFromUser()
+    {}
+}
