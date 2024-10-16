352757e4541c637b9c4c1b1e2d767e6fcd3baa66
loisemusungu <mwongelimusungu@gmail.com>
2024-10-16 12:18:44 +0300

    added the private variambles and methods for this class

---
+++ prove/Develop05/Activity.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Activity
+{
+    protected string _name = "";
+    protected string _description = "";
+    protected int _duration = 0;
+
+    Activity()
+    {}
+
+    public void DisplayStartingMessage()
+    {}
+
+    public void DisplayEndingMessage()
+    {}
+
+    public void ShowSpinner()
+    {}
+
+    public void ShowCountDown()
+    {}
+}
