6a2bc3f92ea2f81647c631b6b8f95bca1c3d517b
loisemusungu <mwongelimusungu@gmail.com>
2024-10-16 12:57:39 +0300

    added private variables and methods

---
+++ prove/Develop05/ReflectingActivity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public class ReflectingActivity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    ReflectingActivity()
+    {}
+
+    public void run()
+    {}
+
+    public void GetRandomPrompt()
+    {}
+
+    public void GetRandomQuestions()
+    {}
+
+    public void DisplayPrompt()
+    {}
+
+    public void DisplayQuestions()
+    {}
+
+
+}
