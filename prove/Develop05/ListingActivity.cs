6a5fb2db9a97982850f5248902516a7daf471c93
loisemusungu <mwongelimusungu@gmail.com>
2024-10-16 12:59:30 +0300

    added system.collections.generic and adjusted the private list

--- prove/Develop05/ListingActivity.cs
+++ prove/Develop05/ListingActivity.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 
 public class ListingActivity
 {
     private int _count = 0;
-    private List<string> _list = new List<string>();
+    private List<string> _prompts;
 
     ListingActivity()
     {}
