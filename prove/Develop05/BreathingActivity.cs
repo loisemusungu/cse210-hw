1e9c5487339694bc1864c820067a9565ab8d4599
loisemusungu <mwongelimusungu@gmail.com>
2024-10-17 12:10:21 +0300

    added functions from the base

--- prove/Develop05/BreathingActivity.cs
+++ prove/Develop05/BreathingActivity.cs
@@ -1,9 +1,18 @@
 using System;
 
-public class BreathingActivity
+public class BreathingActivity: Activity
 {
-    BreathingActivity()
-    {}
+    public BreathingActivity()
+    {
+        SetName("Breathing Activity");
+        SetDescription("This activity will help you relax by walking your through breathing in and out slowly.\n Clear your mind and focus on your breathing.");
+        ShowSpinner();
+        DisplayStartingMessage();
+        Console.WriteLine($"Breath in... {ShowCountDown}");
+        Console.WriteLine($"Breath out... {ShowCountDown}");
+        ShowSpinner();
+        DisplayEndingMessage();
+    }
 
     public void run()
     {}
