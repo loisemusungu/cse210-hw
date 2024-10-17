a35c413c9f03709eb1ecc23e5d6e69211852bf0f
loisemusungu <mwongelimusungu@gmail.com>
2024-10-17 15:05:47 +0300

    changed the breathing activity function and added inherited function in the run function

--- prove/Develop05/BreathingActivity.cs
+++ prove/Develop05/BreathingActivity.cs
@@ -5,15 +5,33 @@ public class BreathingActivity: Activity
     public BreathingActivity()
     {
         SetName("Breathing Activity");
-        SetDescription("This activity will help you relax by walking your through breathing in and out slowly.\n Clear your mind and focus on your breathing.");
-        Console.WriteLine(DisplayStartingMessage());
+        SetDescription("This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
+    }
+
+    public void run()
+    {
+        DisplayStartingMessage();
+
         ShowSpinner();
-        Console.WriteLine($"Breath in... {ShowCountDown}");
-        Console.WriteLine($"Breath out... {ShowCountDown}");
+
+    // User specifies the duration
+    int duration = GetDuration();
+    DateTime endTime = DateTime.Now.AddSeconds(duration);
+
+    while (DateTime.Now < endTime)
+    {
+        Console.WriteLine($"Breathe in... ");  // Countdown for 5 seconds)
+        ShowCountDown(); // Countdown for 5 seconds
+        if (DateTime.Now >= endTime) break; // Check if time is up
+
+        Console.WriteLine("Breathe out... ");
+        ShowCountDown(); // Countdown for 5 seconds
+        if (DateTime.Now >= endTime) break; // Check if time is up
+    }
+
+        Console.WriteLine("This activity is now complete.");
         ShowSpinner();
         Console.WriteLine(DisplayEndingMessage());
     }
 
-    public void run()
-    {}
 }
