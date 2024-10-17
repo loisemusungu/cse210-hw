1dd57ffe1e2f5729689b198c1bbbde5dc60f4ec5
loisemusungu <mwongelimusungu@gmail.com>
2024-10-17 11:35:44 +0300

    testing the spinner

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Activity activity = new Activity();
+        activity.ShowSpinner();
+        activity.ShowCountDown();
         Console.WriteLine("Hello Develop05 World!");
     }
 }
