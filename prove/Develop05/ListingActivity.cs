53586948c3c5be069445ef9bb1dc1afa2ada1a68
loisemusungu <mwongelimusungu@gmail.com>
2024-10-17 19:52:15 +0300

    added count for the listed items

--- prove/Develop05/ListingActivity.cs
+++ prove/Develop05/ListingActivity.cs
@@ -1,20 +1,68 @@
 using System;
 using System.Collections.Generic;
 
-public class ListingActivity
+public class ListingActivity : Activity
 {
     private int _count = 0;
-    private List<string> _prompts;
+    private List<string> _prompts = new List<string>()
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
 
     public ListingActivity()
-    {}
+    {
+        SetName("Listing Activity");
+        SetDescription("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+    }
 
     public void run()
-    {}
+    {
+        DisplayStartingMessage();
 
-    public void GetRandomPrompt()
-    {}
+        ShowSpinner();
 
-    public void GetListFromUser()
-    {}
+        string prompt = GetRandomPrompt();
+        Console.WriteLine($"List as many things as you can for the following prompt:\n\n\"{prompt}\"");
+        Console.WriteLine("You may begin in:");
+        ShowCountDown();  // Countdown before starting to think about the prompt
+
+        // Start accepting input from user and count items listed
+        int itemsListed = GetListFromUser();
+
+        Console.WriteLine($"You listed {itemsListed} items.");
+
+        Console.WriteLine("This activity is now complete.");
+        ShowSpinner();
+        Console.WriteLine(DisplayEndingMessage());
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+
+    private int GetListFromUser()
+    {
+        List<string> list = new List<string>();
+        int duration = GetDuration();
+        DateTime endTime = DateTime.Now.AddSeconds(duration);
+        
+        Console.WriteLine("Start listing items: ");
+        while (DateTime.Now < endTime)
+        {
+            string item = Console.ReadLine();
+            if (!string.IsNullOrEmpty(item))
+            {
+                list.Add(item);
+            }
+        }
+
+        return list.Count;
+    }
 }
