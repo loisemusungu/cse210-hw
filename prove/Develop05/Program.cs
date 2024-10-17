097bc7a81df35bd1abc3e203f1c7d2c622f42f87
loisemusungu <mwongelimusungu@gmail.com>
2024-10-17 19:52:43 +0300

    added a while loop for the menu to continue displaying until the user selects quit

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,41 +1,47 @@
 using System;
+using System.ComponentModel.Design;
 
 class Program
 {
     static void Main(string[] args)
     {   
-        Console.WriteLine("Menu Options");
-        Console.WriteLine("1. Start Breathing Activity");
-        Console.WriteLine("2. Start Reflecting Activity");
-        Console.WriteLine("3. Start Listing Activity");
-        Console.WriteLine("4. QUIT");
+        string selection ="";
 
-        Console.Write("Enter your selection: ");
-        string selection = Console.ReadLine();
+        while (selection != "4")
+        {
+            
+            Console.WriteLine("Menu Options");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflecting Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. QUIT");
 
-        if (selection == "1")
-        {
-            BreathingActivity breathingActivity = new BreathingActivity();
-            breathingActivity.run();
+            Console.Write("Enter your selection: ");
+            selection = Console.ReadLine();
+
+            if (selection == "1")
+            {
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.run();
+            }
+            else if (selection == "2")
+            {
+                ReflectingActivity reflectingActivity = new ReflectingActivity();
+                reflectingActivity.run();
+            }
+            else if (selection == "3")
+            {
+                ListingActivity listingActivity = new ListingActivity();
+                listingActivity.run();
+            }
+            else if (selection == "4")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection. Please try again.");
+            }
         }
-        else if (selection == "2")
-        {
-            ReflectingActivity reflectingActivity = new ReflectingActivity();
-            reflectingActivity.run();
-        }
-        else if (selection == "3")
-        {
-            ListingActivity listingActivity = new ListingActivity();
-            listingActivity.run();
-        }
-        else if (selection == "4")
-        {
-            Console.WriteLine("Goodbye!");
-        }
-        else
-        {
-            Console.WriteLine("Invalid selection. Please try again.");
-        }
-    
     }
 }
