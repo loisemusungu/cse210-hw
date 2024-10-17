e44c03d1b702101991045b95715f581dc9b2d049
loisemusungu <mwongelimusungu@gmail.com>
2024-10-17 11:44:39 +0300

    tested the time count down

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Activity activity = new Activity();
+        activity.SetDuration(10);
         activity.ShowSpinner();
         activity.ShowCountDown();
         Console.WriteLine("Hello Develop05 World!");
