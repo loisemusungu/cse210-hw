55c61535b428941b69ddb90f7d24aa192ab101c3
loisemusungu <mwongelimusungu@gmail.com>
2024-10-17 19:18:57 +0300

    I added the prompts and question list. Added functions to select randomly and display it to the user

--- prove/Develop05/ReflectingActivity.cs
+++ prove/Develop05/ReflectingActivity.cs
@@ -1,28 +1,103 @@
 using System;
 using System.Collections.Generic;
 
-public class ReflectingActivity
+public class ReflectingActivity : Activity
 {
-    private List<string> _prompts;
-    private List<string> _questions;
-
+    private List<string> _prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Reflect on a time when you felt overwhelmed. How did you cope with the situation?",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you helped someone else.",
+        "Think about something you have learned about yourself recently.",
+        "Reflect on a time when you felt like giving up.",
+        "Think about a mistake you have made recently",
+        "Think of a time when you felt confident in yourself.",
+        " Think about a challenge you are currently facing."
+    };
+    private List<string>_questions = new List<string>
+    {
+        "What did you learn from this experience?",
+        "How did you feel during this experience?",
+        "What did you do to overcome the challenge?",
+        "How did you feel after the experience?",
+        "What would you do differently next time?",
+        "What strengths did you use during this experience?",
+        "What did you learn about yourself?",
+        "How did you feel about yourself during this experience?",
+        "What did you learn about others during this experience?",
+        "What did you learn about the situation?"
+    };
+    
     public ReflectingActivity()
-    {}
+    {
+        SetName("Reflecting Activity");
+        SetDescription("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+    }
 
     public void run()
-    {}
+    {
+        DisplayStartingMessage();
 
-    public void GetRandomPrompt()
-    {}
+        ShowSpinner();
 
-    public void GetRandomQuestions()
-    {}
+        // Get a random prompt and display it
+        DisplayPrompt(GetRandomPrompt());
 
-    public void DisplayPrompt()
-    {}
+        // Get a random question and display it
+        DisplayQuestions(GetRandomQuestions());
 
-    public void DisplayQuestions()
-    {}
+    }
 
+    private string GetRandomPrompt()
+    { 
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        return _prompts[index];
 
+    }
+
+    private string GetRandomQuestions()
+    {
+        Random random = new Random();
+        int index = random.Next(_questions.Count);
+        return _questions[index];
+    }
+
+    public void DisplayPrompt(string prompt)
+    {
+        Console.WriteLine("\n\nConsider the following prompt: \n\n");
+        Console.WriteLine(prompt);
+
+        // I want the user to press enter when they are ready to see the questions
+        Console.WriteLine("\n\nWhen you have something in mind, press enter to continue.");
+        Console.ReadLine();
+
+    }
+
+    public void DisplayQuestions(string question)
+    {
+        Console.WriteLine("Here are some questions to help you reflect on your experience: \n\n");
+        Console.Write("You may begin in: \n\n"); 
+        ShowCountDown();
+        Console.WriteLine(question);
+
+        int duration = GetDuration();
+        DateTime endTime = DateTime.Now.AddSeconds(duration); 
+        
+        while (DateTime.Now < endTime)
+        {
+            question = GetRandomQuestions();
+            Console.WriteLine(question);
+            ShowCountDown();
+            if (DateTime.Now >= endTime) break;
+        }
+
+        Console.WriteLine("This activity is now complete.");
+        ShowSpinner();
+        Console.WriteLine(DisplayEndingMessage());
+        
+    }
+    
 }
