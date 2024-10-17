4b8a51a600d2a6a59076d1b492c0bc3dbd2dd2dc
loisemusungu <mwongelimusungu@gmail.com>
2024-10-17 10:55:29 +0300

    added setters and getters and the spinner

--- prove/Develop05/Activity.cs
+++ prove/Develop05/Activity.cs
@@ -6,17 +6,61 @@ public class Activity
     protected string _description = "";
     protected int _duration = 0;
 
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public int GetDuration()
+    {
+        return _duration;
+    }
+        
+    public void SetDuration(int duration)
+    {
+        _duration = duration;
+    }
+
     Activity()
     {}
 
-    public void DisplayStartingMessage()
-    {}
+    public string DisplayStartingMessage()
+    {
+        return $"Welcome to the {_name}.\n {_description}.\n How long, in seconds, would you like for your session?";
+    }
 
-    public void DisplayEndingMessage()
-    {}
+    public string DisplayEndingMessage()
+    {
+        return $"Thank you for using the {_name}. We hope you enjoyed your {_duration} seconds of {_description}.";
+    }
 
     public void ShowSpinner()
-    {}
+    {
+        List<string> spinner = new List<string> { "[", "_", "]", "-" };
+
+        foreach (string s in spinner)
+        {
+            Console.Write(s);
+            System.Threading.Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+
+    }
 
     public void ShowCountDown()
     {}
