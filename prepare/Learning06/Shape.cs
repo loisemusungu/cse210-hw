cd9c63ad66dc1fc5487c74cbed019adbf29dadc2
loisemusungu <mwongelimusungu@gmail.com>
2024-10-21 16:13:03 +0300

    created the bse class, added the getters and setters. And added the virtual method

---
+++ prepare/Learning06/Shape.cs
@@ -0,0 +1,27 @@
+using System;
+
+// This is the base class for calculating ares for all shapes
+
+public class shapes
+{
+    protected string _color;
+
+    public shapes()
+        {
+        }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
