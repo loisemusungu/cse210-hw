b7111205a60202fc7c9e164b6ad3669d03303f21
loisemusungu <mwongelimusungu@gmail.com>
2024-10-21 16:52:32 +0300

    added the list for the shapes to calculate the area

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
+
+//Polymorphism
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square square = new Square(5);
+        Rectangle rectangle = new Rectangle(5, 10);
+        Circle circle = new Circle(5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
