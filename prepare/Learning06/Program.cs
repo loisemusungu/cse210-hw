80a9768bb129f345cc6f57dd557d75268ca2ed55
loisemusungu <mwongelimusungu@gmail.com>
2024-10-21 17:00:58 +0300

    added getcolor to get the color of the shapes

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -8,8 +8,11 @@ class Program
     static void Main(string[] args)
     {
         Square square = new Square(5);
+        square.SetColor("Red");
         Rectangle rectangle = new Rectangle(5, 10);
+        rectangle.SetColor("Blue");
         Circle circle = new Circle(5);
+        circle.SetColor("Green");
 
         List<Shape> shapes = new List<Shape>();
         shapes.Add(square);
@@ -18,7 +21,8 @@ class Program
 
         foreach (Shape shape in shapes)
         {
-            Console.WriteLine(shape.GetArea());
+            Console.WriteLine($"The area is {shape.GetArea()} and has a color of {shape.GetColor()}");
+    
         }
     }
 }
