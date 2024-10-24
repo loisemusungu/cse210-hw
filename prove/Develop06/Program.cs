79c165c6360ae0292cc11d5048cc278b4404b9d7
loisemusungu <mwongelimusungu@gmail.com>
2024-10-24 13:18:30 +0300

    Instatiated the goalmanager and added the loop for the menu

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -4,6 +4,75 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop06 World!");
+        GoalManager goalManager = new GoalManager();
+        string selection ="";
+
+        while (selection != "6")
+        {
+            Console.WriteLine("\n\nWelcome to the Ternal Quest Demo App");
+            Console.WriteLine();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Create a new Goal");
+            Console.WriteLine("2. List your Goals");
+            Console.WriteLine("3. Save your Goals");
+            Console.WriteLine("4. Load your Goals");
+            Console.WriteLine("5. Record an Event");
+            Console.WriteLine("6. Quit");
+
+            Console.Write("Enter your selection: ");
+            selection = Console.ReadLine();
+
+            if (selection == "1")
+            {
+                Console.WriteLine("What type of goal would you like to create?");
+                Console.WriteLine("1. Simple Goal");
+                Console.WriteLine("2. Eternal Goal");
+                Console.WriteLine("3. Checklist Goal");
+                Console.Write("Enter your selection: ");
+                string goalSelection = Console.ReadLine();
+
+                if (goalSelection == "1")
+                {
+                    SimpleGoal simpleGoal = new SimpleGoal();
+                }
+                else if (goalSelection == "2")
+                {
+                    EternalGoal eternalGoal = new EternalGoal();
+                }
+                else if (goalSelection == "3")
+                {
+                    ChecklistGoal checklistGoal = new ChecklistGoal();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection. Please try again.");
+                }
+            }
+            else if (selection == "2")
+            {
+                goalManager.ListGoalNames();
+            }
+            else if (selection == "3")
+            {
+                goalManager.SaveGoals();
+            }
+            else if (selection == "4")
+            {
+                goalManager.LoadGoals();
+    
+            }
+            else if (selection == "5")
+            {
+                goalManager.RecordEvent();
+            }
+            else if (selection == "6")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection. Please try again.");
+            }
+        }
     }
 }
