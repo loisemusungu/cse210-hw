eeae024bac65ea8455a533c3b559570a46e96600
loisemusungu <mwongelimusungu@gmail.com>
2024-10-29 21:19:28 +0300

    added swimming details

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -7,7 +7,9 @@ class Program
     {
         List<Exercise> exercises = new List<Exercise>();
         exercises.Add(new Running(3, 30));
-        exercises.Add(new Running(5.5, 45));
+        exercises.Add(new Cycling(10, 60));
+        exercises.Add(new Swimming(5, 120));
+
 
         foreach (Exercise exercise in exercises)
         {
