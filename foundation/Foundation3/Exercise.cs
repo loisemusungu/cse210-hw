6d99073cb2387cd3b581a59ee10bff38ea3a8e8b
loisemusungu <mwongelimusungu@gmail.com>
2024-10-29 20:26:42 +0300

    created the base class and added the variables, setters and getters and methods

---
+++ foundation/Foundation3/Exercise.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+public class Exercise
+{
+    protected int _distance = 0;
+    protected int _time = 0;
+    protected DateTime currentDate = DateTime.Now;
+
+    public int GetDistance()
+    {
+        return _distance;
+    }
+
+    public void SetDistance(int distance)
+    {
+        _distance = distance;
+    }
+
+    public int GetTime()
+    {
+        return _time;
+    }
+
+    public void SetTime(int time)
+    {
+        _time = time;
+    }
+
+    public double GetSpeed()
+    {
+        return (double)(_distance / _time) * 60;
+    }
+
+    public double GetPace()
+    {
+        return (double)_time / _distance;
+    }
+
+    public virtual void GetSummary()
+    {
+        Console.WriteLine($"Date: {currentDate} - Distance {_distance} miles, Speed {GetSpeed()} mph, Pace {GetPace()} minutes per mile");
+
+    }
+}
