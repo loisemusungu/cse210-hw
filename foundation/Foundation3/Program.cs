287c0e8f5985adc6cd2ccf362f3d25b4827d4eb6
loisemusungu <mwongelimusungu@gmail.com>
2024-10-29 21:03:39 +0300

    added the data

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        List<Exercise> exercises = new List<Exercise>();
+        exercises.Add(new Running(3, 30));
+        exercises.Add(new Running(5.5, 45));
+
+        foreach (Exercise exercise in exercises)
+        {
+            exercise.GetSummary();
+        }
     }
 }
